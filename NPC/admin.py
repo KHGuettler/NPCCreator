4b3712574ab2f2ec7ccc1e36a5c3685a80de9690
KHGuettler <knutguettler@gmail.com>
2021-01-27 12:31:04 +0100

    Updates to Class

--- NPC/admin.py
+++ NPC/admin.py
@@ -1,9 +1,19 @@
 from django.contrib import admin
 
-from .models import Character, Race, Class
+from .models import *
 
 # Register your models here.
 
-admin.site.register(Character)
+# admin.site.register(Character)
 admin.site.register(Race)
 admin.site.register(Class)
+# admin.site.register(Proficiency)
+admin.site.register(ProficiencyType)
+
+@admin.register(Proficiency)
+class ProficiencyAdmin(admin.ModelAdmin):
+    list_display = ('name', 'profType')
+
+@admin.register(Character)
+class CharacterAdmin(admin.ModelAdmin):
+    list_display = ('name', 'npcRace', 'npcClass', 'npcLevel')
--- NPC/models.py
+++ NPC/models.py
@@ -1,7 +1,29 @@
+from django import forms
+from django.core import validators
 from django.db import models
 
 # Create your models here.
 
+
+
+class ProficiencyType(models.Model):
+    profType = models.CharField(max_length=100)
+
+    class Meta:
+        ordering = ['profType']
+
+    def __str__(self):
+        return self.profType
+
+class Proficiency(models.Model):
+    profType = models.ForeignKey(ProficiencyType, on_delete=models.PROTECT)
+    name = models.CharField(max_length=100)
+
+    class Meta:
+        ordering = ['profType']
+
+    def __str__(self):
+        return self.name + self.profType.__str__()
 
 class Race(models.Model):
     raceName = models.CharField(max_length=50)
@@ -11,6 +33,9 @@ class Race(models.Model):
 
 
 class Class(models.Model):
+    class Meta:
+        ordering = ['className']
+
     className = models.CharField(max_length=50)
 
     class HitDice(models.IntegerChoices):
@@ -21,16 +46,31 @@ class Class(models.Model):
 
     hitDice = models.IntegerField(choices=HitDice.choices, null=True)
 
+    proficiencies = models.ManyToManyField(ProficiencyType)
+
+
+
     def __str__(self):
         return self.className
+
+
+
 
 
 class Character(models.Model):
     name = models.CharField(max_length=100)
 
+    npcRace = models.ForeignKey(Race, on_delete=models.CASCADE, verbose_name='Race', null=True)
+
     npcClass = models.ForeignKey(Class, on_delete=models.CASCADE, verbose_name='Class', null=True)
 
-    npcRace = models.ForeignKey(Race, on_delete=models.CASCADE, verbose_name='Race', null=True)
+    npcLevel = models.IntegerField(
+        default=1,
+        validators=[
+            validators.MinValueValidator(1),
+            validators.MaxValueValidator(20),
+        ]
+    )
 
     def __str__(self):
         return self.name
