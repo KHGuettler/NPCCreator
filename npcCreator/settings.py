2759d13e7a1cfd743c6eb80cc38dd19a6fd2803f
KHGuettler <knutguettler@gmail.com>
2021-01-27 16:35:31 +0100

    Update settings.py

--- npcCreator/settings.py
+++ npcCreator/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '4!dzm8zga337bt76uo#zqot@e&c+4#c(y0hy+%!boq#%5qhj2='
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
